25f41e860d09adb02f6293667341a05c8c6f5cb1
Ngong Deland Ngong <delandngong@gmail.com>
2026-07-22 01:05:21 +0300

    configure custom user admin and create test seed accounts

--- advising/admin.py
+++ advising/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Appointment, CaseLog
 
-# Register your models here.
+# 1. Custom User Admin display
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Attributes', {'fields': ('role', 'department')}),
+    )
+    list_display = ['username', 'email', 'role', 'department', 'is_staff']
+
+# 2. Register all models to the dashboard
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Appointment)
+admin.site.register(CaseLog)
